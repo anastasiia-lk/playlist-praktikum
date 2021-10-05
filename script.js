bc37812a0330d4987bb6607deb431b40e4a98588
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2021-10-05 15:33:06 +0300

    Added attributes

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ let container = document.querySelector('.container');
 let songsContainer = container.querySelector('.songs-container');
 let songs = songsContainer.querySelectorAll('.song');
 let addButton = container.querySelector('.form__submit-btn_action_add');
-let resetButton = container.querySelector('.form__submit-btn_action_reset')
+let resetButton = container.querySelector('.form__submit-btn_action_reset');
+if (songs.length === 0) {
+  resetButton.setAttribute('disabled', 'сделать неактивной кнопку');
+  resetButton.setAttribute('style', 'background-color: #f1f1f1');
+}
