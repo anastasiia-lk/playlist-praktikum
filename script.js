0fdad57eb7849e08c4d09e1752c68ae347d05d03
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2021-10-05 09:38:18 +0300

    Added style, script

---
+++ script.js
@@ -0,0 +1,5 @@
+let container = document.querySelector('.container');
+let songsContainer = container.querySelector('.songs-container');
+let songs = songsContainer.querySelectorAll('.song');
+let addButton = container.querySelector('.form__submit-btn_action_add');
+let resetButton = container.querySelector('.form__submit-btn_action_reset')
